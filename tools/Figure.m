17d53c6cc8fb4eec3750d345d45595de25fbf783
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-04-07 13:53:42 +0100

    Add tiling figure window support

--- tools/Figure.m
+++ tools/Figure.m
@@ -10,46 +10,93 @@ classdef Figure < handle
 	%---------------------------- Public Properties ---------------------------%
 	properties
 		Handle		% Handle for root figure
-		Axes		% Handle for root axes within figure
+		Axes		% Handle for root axes within 
+		ActiveAxes	% Selected target axes
 	end
 	
 	properties (Dependent)
 		Title		% Graph title
+		XLabel		% X-Axis label (LaTex)
+		YLabel		% Y-Axis label (LaTex)
+		
+		SuperTitle	% Set title for top of figure
+		XLabels		% Set X-Axis label (LaTex) for all graphs
+		YLabels		% Set Y-Axis label (LaTex) for all graphs
 	end
-	
+
 	%---------------------------- Private Properties --------------------------%
 	properties (Access = private)
 
 	end
 
+	properties (Access = private, Dependent)
+
+	end
+
 	%------------------------------- Constructor ------------------------------%
 	methods
-		function obj = Figure()
+		function obj = Figure(dimensions)
 			% Create figure framework
 			obj.Handle = figure();
-			obj.Axes = axes(obj.Handle);
-			obj.Axes.NextPlot = 'add';
-			obj.Axes.FontName = 'Times';
-			grid(obj.Axes, 'on');
+
+			if(nargin > 0)
+				% Create array of pre-formatted axes
+				obj.Axes = arrayfun(@obj.createAxes, zeros(dimensions(1)*dimensions(2), 1));
+
+				for i = 1:size(obj.Axes, 1)
+					subplot(dimensions(1), dimensions(2), i, obj.Axes(i));
+				end
+			else
+				obj.Axes(1) = obj.createAxes();
+			end
+
+			obj.ActiveAxes = 1;
 		end
 	end
 
 	%------------------------------ Public Methods ----------------------------%
 	methods
 		function handle = plot(obj, varargin)
-			handle = plot(obj.Axes, varargin{:});
+			handle = plot(obj.Axes(obj.ActiveAxes), varargin{:});
 		end
 	end
 		
 	%------------------------------ Private Methods ---------------------------%
 	methods
-		
+		function handle = createAxes(obj, varargin)
+			handle = axes(obj.Handle);
+			handle.NextPlot = 'add';
+			set(handle, 'TickLabelInterpreter', 'latex');
+			grid(handle, 'on');
+		end
 	end
 
 	%------------------------------ Get/Set Methods ---------------------------%
 	methods
 		function set.Title(obj, val)
-			title(obj.Axes, val);
+			title(obj.Axes(obj.ActiveAxes), val, 'interpreter', 'latex');
+		end
+
+		function set.XLabel(obj, val)
+			xlabel(obj.Axes(obj.ActiveAxes), val, 'interpreter', 'latex');
+		end
+
+		function set.YLabel(obj, val)
+			ylabel(obj.Axes(obj.ActiveAxes), val, 'interpreter', 'latex');
+		end
+
+		function set.SuperTitle(obj, val)
+			if(length(obj.Axes) > 1)
+				sgtitle(obj.Handle, val, 'interpreter', 'latex');
+			end
+		end
+
+		function set.XLabels(obj, val)
+			xlabel(obj.Axes, val, 'interpreter', 'latex');
+		end
+
+		function set.YLabels(obj, val)
+			ylabel(obj.Axes, val, 'interpreter', 'latex');
 		end
 	end
 
