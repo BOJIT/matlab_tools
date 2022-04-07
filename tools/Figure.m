bef1ac962f0f597ec235ca4bc190c9c5798709b3
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-04-07 13:03:43 +0100

    Basic figure class plotting

--- tools/Figure.m
+++ tools/Figure.m
@@ -12,7 +12,11 @@ classdef Figure < handle
 		Handle		% Handle for root figure
 		Axes		% Handle for root axes within figure
 	end
-
+	
+	properties (Dependent)
+		Title		% Graph title
+	end
+	
 	%---------------------------- Private Properties --------------------------%
 	properties (Access = private)
 
@@ -21,13 +25,20 @@ classdef Figure < handle
 	%------------------------------- Constructor ------------------------------%
 	methods
 		function obj = Figure()
-
+			% Create figure framework
+			obj.Handle = figure();
+			obj.Axes = axes(obj.Handle);
+			obj.Axes.NextPlot = 'add';
+			obj.Axes.FontName = 'Times';
+			grid(obj.Axes, 'on');
 		end
 	end
 
 	%------------------------------ Public Methods ----------------------------%
 	methods
-		
+		function handle = plot(obj, varargin)
+			handle = plot(obj.Axes, varargin{:});
+		end
 	end
 		
 	%------------------------------ Private Methods ---------------------------%
@@ -37,7 +48,9 @@ classdef Figure < handle
 
 	%------------------------------ Get/Set Methods ---------------------------%
 	methods
-		
+		function set.Title(obj, val)
+			title(obj.Axes, val);
+		end
 	end
 
 end
