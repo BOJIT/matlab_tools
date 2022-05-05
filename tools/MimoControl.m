c10280568565975d98f5ae45a887a7a3e91aeda7
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-05-05 10:11:34 +0100

    Add template for MIMO Control theory

---
+++ tools/MimoControl.m
@@ -0,0 +1,50 @@
+% FILE:         MimoControl.m
+% DESCRIPTION:  MIMO (WM363 Control Theory) Utils Class
+% AUTHOR:       James Bennion-Pedley
+% DATE CREATED: 05/05/2022
+
+%------------------------------------------------------------------------------%
+
+classdef MimoControl < handle
+
+    %---------------------------- Public Properties ---------------------------%
+    properties
+
+    end
+
+    properties (Dependent)
+
+    end
+
+    %---------------------------- Private Properties --------------------------%
+    properties (Access = private)
+
+    end
+
+    properties (Access = private, Dependent)
+
+    end
+
+    %------------------------------- Constructor ------------------------------%
+    methods
+        function obj = MimoControl()
+
+        end
+    end
+
+    %------------------------------ Public Methods ----------------------------%
+    methods
+
+    end
+
+    %------------------------------ Private Methods ---------------------------%
+    methods
+
+    end
+
+    %------------------------------ Get/Set Methods ---------------------------%
+    methods
+
+    end
+
+end
