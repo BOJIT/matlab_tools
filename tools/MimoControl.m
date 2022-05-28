484739b8fa43aaa2ecff935c0597ff101690e87a
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-05-28 21:01:07 +0100

    Working State-Space matrix calculations

--- tools/MimoControl.m
+++ tools/MimoControl.m
@@ -19,13 +19,22 @@ classdef MimoControl < handle
         Y;  % y
     end
 
+    % These properties are used for LaTex wrapping
     properties (Dependent)
         ISOForm;
+        StateSpace;
+
+        % State Space Matrices (for reference purpose only)
+        StateA;
+        StateB;
+        StateC;
+        StateD;
     end
 
     %---------------------------- Private Properties --------------------------%
     properties (Access = private)
         p_ISOForm;
+        p_C;            % Backup of constant symbolic variables
     end
 
     properties (Access = private, Dependent)
@@ -54,7 +63,42 @@ classdef MimoControl < handle
 
     %------------------------------ Public Methods ----------------------------%
     methods
+        function numericConstants(obj, t)
+            if t
+                obj.p_C = obj.C;
+                obj.C = obj.Constants;
+            else
+                obj.C = obj.p_C;
+            end
+        end
 
+        function e = findEquilibriumPoints(obj, constraints)
+            const_state = sym(zeros(numel(obj.Q), 1));
+            for i = 1:numel(obj.Q)
+                const_state(i) = (0 == obj.p_ISOForm(i));
+            end
+
+            % Set temporary constraints
+            for cs = constraints
+                assume(cs);
+            end
+
+            fprintf("Finding Equilibrium Points...");
+            e = solve(const_state, obj.Q, 'ReturnConditions',true);
+            fprintf("Complete!\n");
+
+            % Clear temporary constraints
+            for cs = constraints
+                assume(cs, 'clear');
+            end
+        end
+
+        function s = StateSpaceModel(obj, q, u)
+            s = obj.StateSpace;
+        end
+
+
+        % Controller-related methods
         function [c, rank] = controllabilityMatrix(obj)
             % TODO
         end
@@ -64,10 +108,6 @@ classdef MimoControl < handle
         end
 
         function s = stateObserver(obj, c)
-            % TODO
-        end
-
-        function showWorkings(obj)
             % TODO
         end
     end
@@ -114,10 +154,43 @@ classdef MimoControl < handle
             obj.p_ISOForm = val;
         end
 
+        function val = get.StateSpace(obj)
+            % Calculate state space matrices whenever required
+            q_iso = obj.p_ISOForm(1:length(obj.Q));
+            y_iso = obj.p_ISOForm(length(obj.Q) + 1:end);
+            val.A = jacobian(q_iso, obj.Q);
+            val.B = jacobian(q_iso, obj.U);
+            val.C = jacobian(y_iso, obj.Q);
+            val.D = 0;      % Feed-forward networks not implemented
+        end
+
+        %------------------------- LaTex Copy Functions -----------------------%
+
         function val = get.ISOForm(obj)
             val = obj.p_ISOForm;
             clipboard('copy', obj.latexBrace(val));
             disp("ISO Form copied to clipboard");
+            disp("-----------------------------------------------------------");
+        end
+
+        function val = get.StateA(obj)
+            val = obj.StateSpace.A;
+            clipboard('copy', obj.latexMatrix(val));
+            disp("State Space 'A' matrix copied to clipboard");
+            disp("-----------------------------------------------------------");
+        end
+
+        function val = get.StateB(obj)
+            val = obj.StateSpace.B;
+            clipboard('copy', obj.latexMatrix(val));
+            disp("State Space 'B' matrix copied to clipboard");
+            disp("-----------------------------------------------------------");
+        end
+
+        function val = get.StateC(obj)
+            val = obj.StateSpace.C;
+            clipboard('copy', obj.latexMatrix(val));
+            disp("State Space 'C' matrix copied to clipboard");
             disp("-----------------------------------------------------------");
         end
     end
