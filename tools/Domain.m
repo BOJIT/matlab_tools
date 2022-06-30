8c569492b4f8fc3d705ad5aabe89cc782d70dd33
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-06-30 18:23:19 +0100

    sym2tf now handles unit delays

--- tools/Domain.m
+++ tools/Domain.m
@@ -38,12 +38,28 @@ classdef Domain < handle
     %------------------------------ Public Methods ----------------------------%
     methods (Static)
         function [out, v] = sym2tf(in)
+            % Set the variable that is used in the TF
             v_all = symvar(in);
-            v = char(v_all(1));
+            v = char(v_all(1)); % Primary symvar assumed to be target
+
+            % Convert exponent terms to time delays if they are present
+            delay_term = 0;
+
+            ch = children(in);
+            for c = ch
+                if has(c{:}, 'exp')
+                    d = -double(subs(log(c{:}), v, 1));
+                    warning("Delay Term Present %.3f", d);
+                    delay_term = delay_term + d;
+                    in = in/c{:};   % Remove exponent terms if present
+                end
+            end
+
+            % Extract Polynomials
             [n, d] = numden(in);
             n_coeff = sym2poly(n);
             d_coeff = sym2poly(d);
-            out = tf(n_coeff, d_coeff);
+            out = tf(n_coeff, d_coeff, 'InputDelay', delay_term);
         end
 
         function out = tf2sym(in, v)
@@ -51,14 +67,27 @@ classdef Domain < handle
 
             sym_var = sym(v);
             out = poly2sym(cell2mat(n), sym_var)/poly2sym(cell2mat(d), sym_var);
+
+            if in.InputDelay ~= 0
+                warning("Re-adding Delay Term: %.3f", in.InputDelay);
+                if v == 's'
+                    disp("ADD S");
+                    out = out * exp(-in.InputDelay*sym_var);
+                elseif v == 'z'
+                    disp("ADD Z");
+
+                end
+            end
         end
 
         function z = s2z(s)
-
+            [s_tf, v] = Domain.sym2tf(s);
+            z = Domain.tf2sym(s_tf, v);
+            disp(s_tf);
         end
 
         function s = z2s(z)
-
+            s = iztrans(z, 'z');
         end
 
         function z_n = zDelayForm(z)
