d8ef8b25999855832d20a57801307c6f41b15f0f
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-07-05 14:05:56 +0100

    Add bandpass filter support

--- tools/Filter.m
+++ tools/Filter.m
@@ -11,9 +11,13 @@ classdef Filter < handle
     properties (Constant)
         n = sym('n');
         W_c = sym('W_c');
+        W_l = sym('W_l');
+        W_h = sym('W_h');
         FirPrototype = struct( ...
             'lowpass', piecewise(Filter.n == 0, Filter.W_c/pi, sin(Filter.W_c*Filter.n)/(Filter.n*pi)), ...
-            'highpass', piecewise(Filter.n == 0, pi - Filter.W_c/pi, -sin(Filter.W_c*Filter.n)/(Filter.n*pi)) ...
+            'highpass', piecewise(Filter.n == 0, (pi - Filter.W_c)/pi, -sin(Filter.W_c*Filter.n)/(Filter.n*pi)), ...
+            'bandpass', piecewise(Filter.n == 0, (Filter.W_h - Filter.W_l)/pi, (sin(Filter.W_h*Filter.n) - sin(Filter.W_l*Filter.n))/(Filter.n*pi)), ...
+            'bandstop', piecewise(Filter.n == 0, (pi - Filter.W_h + Filter.W_l)/pi, (sin(Filter.W_l*Filter.n) - sin(Filter.W_h*Filter.n))/(Filter.n*pi)) ...
         );
     end
 
@@ -26,12 +30,43 @@ classdef Filter < handle
 
     %------------------------------ Public Methods ----------------------------%
     methods (Static)
-        function plotImpulseResponse(in_coeffs, out_coeffs)
-
+        function impulseResponse(in_coeffs, out_coeffs)
+            % f = Figure();
+            % f.Title = sprintf("FIR Coefficients for order %u filter", order);
+            % f.XLabel = "Coefficient (non-causal)";
+            % f.YLabel = "Magnitude";
+            % f.stem(n_vals, y_vals, 'b');
         end
 
-        function plotFrequencyResponse(in_coeffs, out_coeffs)
+        function freqResponse(F_s, n_coeffs, d_coeffs)
+            % Allow operation for both FIR and IIR
+            if nargin < 3
+                d_coeffs = 1;
+            end
 
+            % Compute details
+            h = freqz(n_coeffs, d_coeffs);
+            h_mag = mag2db(abs(h));
+            h_phase = rad2deg(angle(h));
+
+            h_freq = linspace(0, F_s/2, length(h_mag));
+
+            f = Figure([2, 1]);
+            f.SuperTitle = "Frequency Response of Digital Filter";
+
+            f.ActiveAxes = 1;
+            f.plot(h_freq, h_mag);
+            f.XLabel = "Frequency / Hz";
+            f.YLabel = "Magnitude / dB";
+
+            xlim(f.Axes(f.ActiveAxes), [0, F_s/2]);
+
+            f.ActiveAxes = 2;
+            f.plot(h_freq, h_phase);
+            f.XLabel = "Frequency / Hz";
+            f.YLabel = "Phase / degrees";
+
+            xlim(f.Axes(f.ActiveAxes), [0, F_s/2]);
         end
 
         function [y_vals, y_windowed] = fir(order, F_c, F_s, type, window)
@@ -43,11 +78,16 @@ classdef Filter < handle
 
             % Evaluate for piecewise 'sinc' function
             syms y(n); y(n) = Filter.FirPrototype.(type);
-            y_vals = double(vpa(subs(y(n_vals), Filter.W_c, W_c), 4));
+
+            if length(F_c) == 1
+                y_vals = double(vpa(subs(y(n_vals), Filter.W_c, W_c), 4));
+            else
+                y_vals = double(vpa(subs(y(n_vals), [Filter.W_l, Filter.W_h], [W_c(1), W_c(2)]), 4));
+            end
 
             % Print stats
             fprintf("n steps: "); disp(vpa(n_vals, 2));
-            fprintf("coefficients: "); disp(vpa(y_vals, 3));
+            fprintf("coefficients: "); disp(vpa(y_vals, 4));
 
             % Create graph with/without windowing function
             f = Figure();
@@ -64,6 +104,10 @@ classdef Filter < handle
 
                 legend({'No window (boxcar)', 'windowed'})
             end
+        end
+
+        function iir()
+
         end
 
         function bilinearTransform(s)
